c764cb57a5dfe24d94cb0cde5ea8bd2160c68693
zeba-chikkumbi <62893751+zeba-chikkumbi@users.noreply.github.com>
2021-03-06 14:53:53 +0530

    Add files via upload

---
+++ courier_db/contact.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Courier / Cargo Tracking Script in PHP - Ver 0.97</title>
+<link href="css/mystyle.css" rel="stylesheet" type="text/css">
+
+
+</head>
+
+<body>
+<table border="0" cellpadding="0" cellspacing="0" align="center" width="900">
+  <tbody><tr>
+    <td width="900">
+	
+<table border="0" cellpadding="0" cellspacing="0" width="900">
+<tbody><tr>
+<td><img src="images/trheader.jpg" height="109" width="900"></td>
+</tr>
+<tr>
+<td bgcolor="#FFCC00">&nbsp;</td>
+</tr>
+</tbody></table>
+	</td>
+  </tr>
+  
+  <tr>
+    <td bgcolor="#FFFFFF"><table border="0" cellpadding="1" cellspacing="1" align="center" width="98%">
+      <tbody><tr>
+        <td class="Partext1">&nbsp;</td>
+      </tr>
+      
+      <tr>
+        <td class="Partext1"><div align="center">
+          <table cellpadding="4" cellspacing="0" align="center" width="70%">
+
+            
+              <tbody><tr>
+                <td class="TrackTitle" valign="top"><div class="newtext" align="center"><strong> <b>Contact-us </b><br>
+                        </strong></div></td>
+              </tr>
+			 
+			   <tr><td><div align="right"><a href="index.php">Back</a></div></td></tr>
+              
+			  <tr bgcolor="EFEFEF">
+                <td class="aalpha" valign="top"><div align="center"><b>sameena <b></div></td>
+              </tr>
+              <tr bgcolor="EFEFEF">
+               
+            <td class="aalpha" valign="top"><div align="center">Phone:9632843708 </div></td>
+
+			     
+                </tr>
+				<tr bgcolor="EFEFEF">
+                <td class="aalpha" valign="top"><div align="center"><b>Sampada Pandurangi  <b></div></td>
+              </tr>
+              <tr bgcolor="EFEFEF">
+               
+            <td class="aalpha" valign="top"><div align="center">Phone:9480179744 </div></td>
+
+			     
+                </tr>
+				<tr bgcolor="EFEFEF">
+                <td class="aalpha" valign="top"><div align="center"><b>Shruti Kalburgi <b></div></td>
+              </tr>
+              <tr bgcolor="EFEFEF">
+               
+            <td class="aalpha" valign="top"><div align="center">Phone:9538047243 </div></td>
+
+			     
+                </tr>
+				<tr bgcolor="EFEFEF">
+                <td class="aalpha" valign="top"><div align="center"><b>Zeba Tahseen Chikkumbi <b></div></td>
+              </tr>
+              <tr bgcolor="EFEFEF">
+               
+            <td class="aalpha" valign="top"><div align="center">Phone:8867767758 </div></td>
+
+			     
+                </tr>
+              <tr bgcolor="EFEFEF">
+                <td valign="top">&nbsp;</td>
+              </tr>
+              <tr bgcolor="EFEFEF">
+                <td class="TrackNormalBlue" bgcolor="#FFFFFF" valign="top">&nbsp;</td>
+              </tr>
+            
+          </tbody></table>
+		 
+        </div></td>
+        </tr>
+      <tr>
+        <td class="Partext1">&nbsp;</td>
+		
+        </tr>
+      
+      <tr>
+        <td>&nbsp;</td>
+        </tr>
+    </tbody></table>      </td>
+  </tr>
+  <tr>
+    <td><table border="0" cellpadding="0" cellspacing="0" align="center" width="900">
+  <tbody><tr>
+    <td bgcolor="#2284d5" height="40" width="476">&nbsp;</td>
+    <td bgcolor="#2284d5" width="304">&nbsp;</td>
+  </tr>
+</tbody></table>
+</td>
+  </tr>
+</tbody></table>
+
+
+</body></html>
